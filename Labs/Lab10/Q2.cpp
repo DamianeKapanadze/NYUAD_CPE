96116bb1344eb3fe13b8111bb8eef273331bcf5b
DamianeKapanadze <75084003+DamianeKapanadze@users.noreply.github.com>
2023-11-24 14:52:50 +0400

    Create Q2.cpp

---
+++ Labs/Lab10/Q2.cpp
@@ -0,0 +1,27 @@
+
+
+class Fraction {
+private:
+    int numerator = 0, denominator = 0;
+
+public:
+
+
+    //member functions
+
+    void addition() {
+
+    }
+
+    void subtraction() {
+
+    }
+
+    void multiplication() {
+
+    }
+
+    void division() {
+
+    }
+};
