b6f2a9966912e4ffbabc4c83131c3a1b35279a34
DamianeKapanadze <75084003+DamianeKapanadze@users.noreply.github.com>
2023-12-12 12:39:40 +0400

    Add files via upload

---
+++ FinalProject/Shoot_Arrow.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot_Arrow : MonoBehaviour
+{
+    public GameObject Arrow_Emitter;
+
+    public GameObject Arrow;
+
+    public float Arrow_Force;
+
+    private float nextTimeToFire = 0f;
+    public float fireingRate = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if((Input.GetKeyUp("space") && Time.time >= nextTimeToFire) || (OVRInput.Get(OVRInput.RawButton.RIndexTrigger) && Time.time >= nextTimeToFire)) {
+            nextTimeToFire = Time.time + 1f / fireingRate;
+            GameObject Temp_Arrow_Handler;
+
+            Temp_Arrow_Handler = Instantiate(Arrow, Arrow_Emitter.transform.position, Arrow_Emitter.transform.rotation) as GameObject;
+
+            //Temp_Arrow_Handler.transform.Rotate(Vector3.up * 90);
+            //Temp_Arrow_Handler.transform.Rotate(Vector3.up * 90);
+
+            Rigidbody Temp_Rigidbody;
+
+            Temp_Rigidbody = Temp_Arrow_Handler.GetComponent<Rigidbody>();
+
+            Temp_Rigidbody.AddForce(transform.right * -Arrow_Force);
+
+            Destroy(Temp_Arrow_Handler, 10f);
+        }
+            
+        
+    }
+}
