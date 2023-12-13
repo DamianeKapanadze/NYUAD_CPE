d707f5f4eb538d58c78a08563fb82a7f4b4f876d
DamianeKapanadze <75084003+DamianeKapanadze@users.noreply.github.com>
2023-12-13 16:19:46 +0400

    Update HealthController.cs

--- FinalProject/HealthController.cs
+++ FinalProject/HealthController.cs
@@ -5,11 +5,11 @@ using UnityEngine.UI;
 
 public class HealthController : MonoBehaviour
 {
-    public Image[] alive;
-    public Image[] dead;
+    public Image[] alive; // drag and drop the alive heart image form canvas
+    public Image[] dead; // draga nd drop dead heart image form canvas
     public static HealthController instance;
 
-    // Start is called before the first frame update
+    // Awake is done before Start, and the instance makes sure that there is only one copy of the script.
     private void Awake()
     {
         instance = this;
@@ -17,6 +17,7 @@ public class HealthController : MonoBehaviour
 
     void Start()
     {
+        //make sure the alives are turned on and dead turned off in the begining
         for(int i = 0; i < alive.Length; i++)
         {
             alive[i].enabled = true;
@@ -24,19 +25,20 @@ public class HealthController : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
+
     public void kill()
     {
+        // go over the whole array
         for (int i = alive.Length-1; i >= 0 ; i--)
         {
-            //Debug.Log(i);
+            //if the alive is turned on, turn it off and turn on the dead image on it's place
+            //The hearts will follow their index in the array (first gets turned off first)
             if(alive[i].enabled == true)
             {
                 dead[i].enabled = true;
                 alive[i].enabled = false;
                 break;
             }
-
         }
     }
 }
