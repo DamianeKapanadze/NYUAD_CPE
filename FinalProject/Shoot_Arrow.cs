e8bafdf9e94b284ac0adfeb1da6b0dae7ecaaad7
DamianeKapanadze <75084003+DamianeKapanadze@users.noreply.github.com>
2023-12-14 17:02:14 +0400

    Update Shoot_Arrow.cs

--- FinalProject/Shoot_Arrow.cs
+++ FinalProject/Shoot_Arrow.cs
@@ -4,42 +4,39 @@ using UnityEngine;
 
 public class Shoot_Arrow : MonoBehaviour
 {
-    public GameObject Arrow_Emitter;
-
-    public GameObject Arrow;
-
-    public float Arrow_Force;
-
-    private float nextTimeToFire = 0f;
+    public GameObject Arrow_Emitter; // attach arrowEmiter object (where the arrow should appear)
+    public GameObject Arrow; // arrow prefab
+    public float Arrow_Force; // force to shoot the arrow
     public float fireingRate = 1f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float nextTimeToFire = 0f;
+    private bool lastGrabbed = false;
 
-    // Update is called once per frame
     void Update()
     {
-        if((Input.GetKeyUp("space") && Time.time >= nextTimeToFire) || (OVRInput.Get(OVRInput.RawButton.RIndexTrigger) && Time.time >= nextTimeToFire)) {
+        // if the hand was grabbing but isn't grabbing now, and it is time to fire
+        if((lastGrabbed == true && GrabbedShare.grabbed == false) && Time.time >= nextTimeToFire) {
+            // update time to fire
             nextTimeToFire = Time.time + 1f / fireingRate;
-            GameObject Temp_Arrow_Handler;
 
+            // create an arrow at arrow_Emmitter position
+            GameObject Temp_Arrow_Handler;
             Temp_Arrow_Handler = Instantiate(Arrow, Arrow_Emitter.transform.position, Arrow_Emitter.transform.rotation) as GameObject;
 
             //Temp_Arrow_Handler.transform.Rotate(Vector3.up * 90);
             //Temp_Arrow_Handler.transform.Rotate(Vector3.up * 90);
 
+            //add rigidbody to the arrow
             Rigidbody Temp_Rigidbody;
-
             Temp_Rigidbody = Temp_Arrow_Handler.GetComponent<Rigidbody>();
-
+            //shoot the arrow
             Temp_Rigidbody.AddForce(transform.right * -Arrow_Force);
 
+            // destroy the arrow in 10 seconds
             Destroy(Temp_Arrow_Handler, 10f);
         }
-            
-        
+
+        // update the lastGrabbed variabel
+        lastGrabbed = GrabbedShare.grabbed;
     }
 }
