b776da2dae83f27e3a01a8968f0e52eaf8ed9958
DamianeKapanadze <75084003+DamianeKapanadze@users.noreply.github.com>
2023-12-14 19:45:37 +0400

    Update HealthController.cs

--- FinalProject/HealthController.cs
+++ FinalProject/HealthController.cs
@@ -8,7 +8,12 @@ public class HealthController : MonoBehaviour
     public Image[] alive; // drag and drop the alive heart image form canvas
     public Image[] dead; // draga nd drop dead heart image form canvas
     public static HealthController instance;
+    public GameObject pause;
+    public GameObject gameOver;
+    public GameObject horse;
+    private bool aliveCheck = false;
 
+    
     // Awake is done before Start, and the instance makes sure that there is only one copy of the script.
     private void Awake()
     {
@@ -35,10 +40,26 @@ public class HealthController : MonoBehaviour
             //The hearts will follow their index in the array (first gets turned off first)
             if(alive[i].enabled == true)
             {
+                // check if the player ran out of lives
+                if (i!=0)
+                {
+                    aliveCheck = true;
+                }
                 dead[i].enabled = true;
                 alive[i].enabled = false;
                 break;
             }
         }
+
+        //check if the player is dead
+        if (aliveCheck == false)
+        {    
+            //go to game over screen, stop everything else
+            pause.SetActive(false);
+            gameOver.SetActive(true);
+            horse.SetActive(false);
+            Time.timeScale = 0f;
+        }
+        
     }
 }
